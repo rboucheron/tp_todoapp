9a34e5e9d8694e364d3dee7717f30cd458a85c48
Raphaël <115149179+rboucheron@users.noreply.github.com>
2025-03-08 23:19:39 +0100

    first commit

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TaskController extends AbstractController
+{
+
+    public function __construct(
+        private readonly TaskRepository $taskRepository,
+        private EntityManagerInterface  $entityManager
+    )
+    {
+    }
+
+    #[Route('/', name: 'app_task')]
+    public function index(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'tasks' => $this->taskRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/task/{id}/edit', name: 'app_task_edit')]
+    public function edit(Task $task, Request $request): Response
+    {
+        $taskForm = $this->createForm(TaskType::class, $task);
+        $taskForm->handleRequest($request);
+        if ($taskForm->isSubmitted() && $taskForm->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_task');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'taskForm' => $taskForm,
+        ]);
+    }
+
+    #[Route('/task/{id}/delete', name: 'app_task_delete')]
+    public function delete(Task $task): Response
+    {
+        $this->entityManager->remove($task);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('app_task');
+    }
+
+    #[Route('/task/add', name: 'app_task_add')]
+    public function add(
+        Request $request
+    ): Response
+    {
+        $taskForm = $this->createForm(TaskType::class);
+        $taskForm->handleRequest($request);
+        if ($taskForm->isSubmitted() && $taskForm->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_task');
+        }
+        return $this->render('task/edit.html.twig', [
+            'controller_name' => 'TaskController',
+            'taskForm' => $taskForm,
+        ]);
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+
+    private $data = [
+        [
+            'title' => 'Task 1',
+            'description' => 'Description 1',
+            'status' => 'new',
+            'created_at' => '2021-01-01 00:00:00',
+        ],
+        [
+            'title' => 'Task 2',
+            'description' => 'Description 2',
+            'status' => 'new',
+            'created_at' => '2021-01-02 00:00:00',
+        ],
+        [
+            'title' => 'Task 3',
+            'description' => 'Description 3',
+            'status' => 'new',
+            'created_at' => '2021-01-03 00:00:00',
+        ],
+    ];
+
+    /**
+     * @throws \DateMalformedStringException
+     */
+    public function load(ObjectManager $manager): void
+    {
+        foreach ($this->data as $item) {
+            $task = new Task();
+            $task->setTitle($item['title']);
+            $task->setDescription($item['description']);
+            $task->setStatus($item['status']);
+            $task->setCreatedAt(new \DateTimeImmutable($item['created_at']));
+
+
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
